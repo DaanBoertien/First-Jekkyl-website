88634b4e534f4dbe95a23c8bed0b76a8e141e238
daanb <daan.boertien@gmail.com>
2020-11-11 22:01:11 +0100

    added gsap

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,3 +5,9 @@ window.onload = function() {
     videoContainer.classList.add("loaded")
     
   }
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.batch(".fadeIn", {
+    onEnter: batch => gsap.from(batch, {duration: 1, y: 30, autoAlpha: 0, stagger: 0.2, ease: Power2.inOut}),
+});
