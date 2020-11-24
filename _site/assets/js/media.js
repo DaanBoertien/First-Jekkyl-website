1ac3ba6f1af60a2fb6a5738cdc24eed959810375
daanb <daan.boertien@gmail.com>
2020-11-24 15:41:18 +0100

    added picture

--- _site/assets/js/media.js
+++ _site/assets/js/media.js
@@ -8,21 +8,26 @@ const modal = document.querySelector('.modal');
 
 allPhotos.addEventListener('click', (e) => {
     console.log(e.target);
+    
     modalImage = document.querySelector('.modal-image');
-    if (modalImage) {
-        modalImage.parentNode.removeChild(modalImage);
-    }
-    let imageAngle;
-    if (window.width > window.height) {
-        imageAngle = 'landscape';
-    }
+    if (body.clientWidth > 700) {
 
-    else {
-        imageAngle = 'portrait';
-    }
+    
+        if (modalImage) {
+            modalImage.parentNode.removeChild(modalImage);
+        }
+        // let imageAngle;
+        // if (modalImage.width > modalImage.height) {
+        //     imageAngle = 'landscape';
+        // }
 
-    modal.insertAdjacentHTML( 'beforeend', `<img src=${e.target.src} class="modal-image ${imageAngle}"></img>` );
-    modal.classList.toggle('modal-open');
+        // else {
+        //     imageAngle = 'portrait';
+        // }
+
+        modal.insertAdjacentHTML( 'beforeend', `<img src=${e.target.src} class="modal-image"></img>` );
+        modal.classList.toggle('modal-open');
+    }
 })
 
 modal.addEventListener('click', () => {
