5eea407d239089e4dade31a0d58a32df951aee1a
daanb <daan.boertien@gmail.com>
2021-01-10 15:45:36 +0100

    adding english functionality

--- assets/js/media.js
+++ assets/js/media.js
@@ -7,26 +7,27 @@ const modal = document.querySelector('.modal');
 
 
 allPhotos.addEventListener('click', (e) => {
-    console.log(e.target);
+    if (e.target.src){
     
-    modalImage = document.querySelector('.modal-image');
-    if (body.clientWidth > 700) {
+        modalImage = document.querySelector('.modal-image');
+        if (body.clientWidth > 700) {
 
-    
-        if (modalImage) {
-            modalImage.parentNode.removeChild(modalImage);
+        
+            if (modalImage) {
+                modalImage.parentNode.removeChild(modalImage);
+            }
+            // let imageAngle;
+            // if (modalImage.width > modalImage.height) {
+            //     imageAngle = 'landscape';
+            // }
+
+            // else {
+            //     imageAngle = 'portrait';
+            // }
+
+            modal.insertAdjacentHTML( 'beforeend', `<img src=${e.target.src} class="modal-image"></img>` );
+            modal.classList.toggle('modal-open');
         }
-        // let imageAngle;
-        // if (modalImage.width > modalImage.height) {
-        //     imageAngle = 'landscape';
-        // }
-
-        // else {
-        //     imageAngle = 'portrait';
-        // }
-
-        modal.insertAdjacentHTML( 'beforeend', `<img src=${e.target.src} class="modal-image"></img>` );
-        modal.classList.toggle('modal-open');
     }
 })
 
